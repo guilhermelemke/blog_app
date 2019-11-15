719aaa01db4558c297c0a4699ef256798e4cdb06
Guilherme Lemke <guilherme.lemke@gmail.com>
2019-11-15 14:53:36 -0300

    Heroku config4

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoise.SessionMiddleware', # CSS static
+    'whitenoise.middleware.WhiteNoiseMiddleware', # CSS static
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
